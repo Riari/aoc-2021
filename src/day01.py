4b92613bc4cbb51a517c80ebcb373a8d0b5c25a1
Riari <3583774+Riari@users.noreply.github.com>
2021-12-01 11:02:49 +0000

    Complete day01

--- src/day01.py
+++ src/day01.py
@@ -1,10 +1,25 @@
 from utils.solution import Solution
 
-def part1(input: list[str]) -> int:
-    return 0
+def part1(input: list[int]) -> int:
+    count = 0
+    for a, b in zip(input, input[1:]):
+        if a < b:
+            count += 1
 
-def part2(input: list[str]) -> int:
-    return 0
+    return count
+
+def part2(input: list[int]) -> int:
+    a, b = 0, 0
+    count = 0
+    window_size = 3
+    for i in range(len(input) - window_size + 1):
+        b = sum(input[i: i + window_size])
+        if a > 0 and b > a:
+            count += 1
+
+        a = b
+
+    return count
 
 if __name__ == "__main__":
     solution = Solution(1, part1, part2, 'day01.txt')
--- src/utils/solution.py
+++ src/utils/solution.py
@@ -7,17 +7,15 @@ class Solution:
     part2: Callable
     input: list[str]
 
-    def __init__(self, day: int, part1: Callable, part2: Callable, input_filename: str):
+    def __init__(self, day: int, part1: Callable, part2: Callable, input_filename: str, input_type = int):
         self.day = day
         self.part1 = part1
         self.part2 = part2
-        self.input = self.__read_input(input_filename)
+        self.input = self.__read_input(input_filename, input_type)
 
-    def __read_input(self, input_filename: str) -> list[str]:
-        file = open(f'input/{input_filename}', 'r')
-        input = file.readlines()
-        file.close()
-        return input
+    def __read_input(self, input_filename: str, input_type = int) -> list:
+        with open(f'input/{input_filename}', 'r') as f:
+            return list(map(input_type, f))
 
     def run(self):
         print(f'## Day {self.day}')
