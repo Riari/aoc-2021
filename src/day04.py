8fd5f197564eef1fbf1ba05a521eb28cd3458f88
Riari <3583774+Riari@users.noreply.github.com>
2021-12-04 11:28:59 +0000

    Complete day04

---
+++ src/day04.py
@@ -0,0 +1,59 @@
+from utils.solution import Solution
+
+def solve(input: list[str], return_first_win: bool) -> int:
+    board_index = -1
+    numbers = []
+    boards = []
+    row_width = 5
+    for i in range(len(input)):
+        line = input[i]
+        if i == 0:
+            numbers = [int(i) for i in line.split(',')]
+        elif line in ['\n', '\r\n']:
+            board_index += 1
+            boards.append([])
+        else:
+            boards[board_index].extend([int(i) for i in line.split()])
+
+    marked_numbers = [set() for _ in range(len(boards))]
+    winning_boards = set()
+    winning_number: int = None
+    for a in range(len(numbers)):
+        number = numbers[a]
+        for i in range(len(boards)):
+            if i in winning_boards:
+                continue
+
+            board = boards[i]
+            for j in range(len(board)):
+                board_number = board[j]
+                if board_number != number:
+                    continue
+
+                marked_numbers[i].add(board_number)
+                
+                y = j // row_width
+                x = j % row_width
+                row = {board[n] for n in range(y * row_width, y * row_width + row_width)}
+                column = {board[n] for n in [x] + [row_width * k + x for k in range(1, 5)]}
+                
+                if row.issubset(marked_numbers[i]) or column.issubset(marked_numbers[i]):
+                    board_set = set(board)
+                    unmarked_numbers = board_set.symmetric_difference(marked_numbers[i])
+                    if return_first_win:
+                        return sum(list(unmarked_numbers)) * number
+
+                    winning_number = number
+                    winning_boards.add(i)
+    else:
+        return sum(list(unmarked_numbers)) * winning_number
+
+def part1(input: list[str]) -> int:
+    return solve(input, True)
+
+def part2(input: list[str]) -> int:
+    return solve(input, False)
+
+if __name__ == "__main__":
+    solution = Solution(4, part1, part2, 'day04.txt', str)
+    solution.run()
