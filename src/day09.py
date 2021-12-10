31175aec7dcb6a22a29c9a1351ad1ede10d2e5be
Riari <3583774+Riari@users.noreply.github.com>
2021-12-10 00:41:55 +0000

    Complete day09 part 2

--- src/day09.py
+++ src/day09.py
@@ -1,6 +1,6 @@
 from utils.solution import Solution
 
-def get_lowpoint_coords(heightmap: list[int]) -> list[tuple]:
+def get_lowpoints(heightmap: list[int]) -> list[tuple]:
     coords = []
     for y in range(len(heightmap)):
         row = heightmap[y]
@@ -15,43 +15,40 @@ def get_lowpoint_coords(heightmap: list[int]) -> list[tuple]:
     return coords
 
 def part1(input: list[int]) -> int:
-    lowpoints = [input[y][x] for x, y in get_lowpoint_coords(input)]
-    return sum(lowpoints) + len(lowpoints)
+    return sum([input[y][x] + 1 for x, y in get_lowpoints(input)])
 
 def part2(input: list[int]) -> int:
-    lowpoint_coords = get_lowpoint_coords(input)
-    basin_coords = [set(tuple()) for _ in range(len(lowpoint_coords))]
+    lowpoints = get_lowpoints(input)
+    basins = [set(tuple()) for _ in range(len(lowpoints))]
     neighbours = [
         ( 0, -1), # N
         ( 1,  0), # E
         ( 0,  1), # S
         (-1,  0), # W
     ]
+    rows, cols = range(len(input)), range(len(input[0]))
 
-    def check_neighbours(i: int, x: int, y: int):
+    def check_neighbours(i: int, x: int, y: int) -> None:
         for h, v in neighbours:
-            nx = x + h
-            ny = y + v
-            if ny < 0 or ny >= len(input): continue
-            if nx < 0 or nx >= len(input[ny]): continue
+            nx, ny = x + h, y + v
+            if ny not in rows or nx not in cols: continue
             if input[ny][nx] == 9: continue
-            if input[ny][nx] != input[y][x] + 1: continue
-            if (nx, ny) in basin_coords[i]: continue
+            if (nx, ny) in basins[i]: continue
 
-            basin_coords[i].add((nx, ny))
+            basins[i].add((nx, ny))
             check_neighbours(i, nx, ny)
 
-    for i in range(len(lowpoint_coords)):
-        x, y = lowpoint_coords[i]
-        basin_coords[i].add((x, y))
+    for i in range(len(lowpoints)):
+        x, y = lowpoints[i]
+        basins[i].add((x, y))
         check_neighbours(i, x, y)
 
-    basin_coords.sort(key = len)
-    basin_coords.reverse()
+    basins.sort(key = len)
+    basins.reverse()
 
-    return len(basin_coords[0]) * len(basin_coords[1]) * len(basin_coords[2])
+    return len(basins[0]) * len(basins[1]) * len(basins[2])
 
 if __name__ == "__main__":
     solution = Solution(9, part1, part2, 'day09.txt')
-    solution.input = [[int(digit) for digit in line] for line in [l for l in solution.input]]
+    solution.input = [[int(digit) for digit in line] for line in solution.input]
     solution.run()
