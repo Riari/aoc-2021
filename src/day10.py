471d93558e1662b571ff39d7c9692d9e08dbed3e
Riari <3583774+Riari@users.noreply.github.com>
2021-12-10 10:35:52 +0000

    Complete day10

--- src/day10.py
+++ src/day10.py
@@ -1,32 +1,35 @@
 from utils.solution import Solution
 
-def part1(input: list[str]) -> int:
-    error_score = 0
-    pairs = {
-        '(': ')',
-        '[': ']',
-        '{': '}',
-        '<': '>',
-    }
+def solve(input: list[str], return_error_score: bool, scores: dict) -> int:
+    pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
+    results = []
     for line in input:
         stack = []
         for c in line:
-            if c in pairs.keys():
-                stack.append(c)
-            elif pairs[stack[-1]] == c:
+            if c in pairs:
+                stack.append(pairs[c])
+            elif stack[-1] == c:
                 stack.pop()
             else:
-                match c:
-                    case ')': error_score += 3
-                    case ']': error_score += 57
-                    case '}': error_score += 1197
-                    case '>': error_score += 25137
+                if return_error_score:
+                    results.append(scores[c])
                 break
+        else:
+            if return_error_score: continue
+            score = 0
+            for c in stack[::-1]:
+                score *= 5
+                score += scores[c]
+            results.append(score)
 
-    return error_score
+    results.sort()
+    return sum(results) if return_error_score else results[len(results) // 2]
+
+def part1(input: list[str]) -> int:
+    return solve(input, True, {')': 3, ']': 57, '}': 1197, '>': 25137})
 
 def part2(input: list[str]) -> int:
-    return 0
+    return solve(input, False, {')': 1, ']': 2, '}': 3, '>': 4})
 
 if __name__ == "__main__":
     solution = Solution(10, part1, part2, 'day10.txt')
