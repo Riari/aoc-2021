3ee3b6c6edb318797ba998cf8a296e54f3acdfe8
Riari <3583774+Riari@users.noreply.github.com>
2021-12-11 14:50:26 +0000

    Refactor day11

--- day11/main.py
+++ day11/main.py
@@ -1,57 +1,51 @@
+from typing import Callable
 from utils.solution import Solution
 
-def solve(input: list[list[int]], part_1: bool) -> int:
+def solve(input: list[list[int]], end_step: Callable) -> int:
     height, width = range(len(input)), range(len(input[0]))
+    coordinates = [(y, x) for y in height for x in width]
 
     def flash(y: int, x: int) -> int:
-        f = 1
+        flashes = 1
         input[y][x] = -1
         for v in [-1, 0, 1]:
             for h in [-1, 0, 1]:
-                ny = y + v
-                nx = x + h
+                ny, nx = y + v, x + h
 
                 if ny not in height or nx not in width or input[ny][nx] == -1:
                     continue
 
                 input[ny][nx] += 1
                 if input[ny][nx] > 9:
-                    f += flash(ny, nx)
+                    flashes += flash(ny, nx)
 
-        return f
+        return flashes
 
     step, flashes = 0, 0
     while True:
         step += 1
         step_flashes = 0
 
-        for y in height:
-            for x in width:
-                input[y][x] += 1
+        for y, x in coordinates: input[y][x] += 1
 
-        for y in height:
-            for x in width:
-                if input[y][x] == 10:
-                    neighbour_flashes = flash(y, x)
-                    step_flashes += neighbour_flashes
-                    flashes += neighbour_flashes
+        for y, x in coordinates:
+            if input[y][x] == 10:
+                neighbour_flashes = flash(y, x)
+                step_flashes += neighbour_flashes
+                flashes += neighbour_flashes
 
-        for y in height:
-            for x in width:
-                if input[y][x] == -1:
-                    input[y][x] = 0
+        for y, x in coordinates:
+            if input[y][x] == -1:
+                input[y][x] = 0
 
-        if part_1 and step == 100:
-            return flashes
-
-        if step_flashes == width.stop * height.stop:
-            return 100 + step # Part 1 did the first 100 steps
+        result = end_step(step, flashes, step_flashes)
+        if result > 0: return result
 
 def part1(input: list[list[int]]) -> int:
-    return solve(input, True)
+    return solve(input, lambda step, flashes, step_flashes: flashes if step == 100 else 0)
 
 def part2(input: list[list[int]]) -> int:
-    return solve(input, False)
+    return solve(input, lambda step, flashes, step_flashes: 100 + step if step_flashes == len(input) * len(input[0]) else 0)
 
 if __name__ == "__main__":
     solution = Solution(11, part1, part2, (1656, 195))
