cda12e8185e02f9a4a54617750340c596054651c
Riari <3583774+Riari@users.noreply.github.com>
2021-12-24 22:11:41 +0000

    Start day22 part 2

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from utils.vector import Vector3
+from __future__ import annotations
 from utils.solution import Solution
 
 class Step:
@@ -14,37 +13,47 @@ class Step:
         self.z = z
         self.state = state
 
-def part1(steps: list[Step]) -> int:
-    reactor = set()
-    region = range(-50, 51)
-    for step in steps:
-        for x in step.x:
-            if x not in region: continue
-            for y in step.y:
-                if y not in region: continue
-                for z in step.z:
-                    if z not in region: continue
-                    vector = Vector3(x, y, z)
-                    if step.state:
-                        reactor.add(vector)
-                    elif vector in reactor:
-                        reactor.remove(vector)
-    
-    return len(reactor)
+class Cuboid:
+    x: range
+    y: range
+    z: range
 
-def part2(steps: list[Step]) -> int:
-    reactor = set()
-    region = range(-50, 51)
+    def __init__(self, x: range, y: range, z: range):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def intersects_with(self, other: Cuboid) -> bool:
+        has_x = other.x.start in self.x or other.x.stop - 1 in self.x
+        if has_x: return True
+        has_y = other.y.start in self.y or other.y.stop - 1 in self.y
+        if has_y: return True
+        return other.z.start in self.z or other.z.stop - 1 in self.z
+
+    def split_by(self, other: Cuboid) -> tuple:
+        # TODO: split into multiple cuboids, minus other
+        return ()
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.z))
+
+def solve(steps: list[Step], init_region = range(-10000, 10000)) -> int:
+    reactor: set[Cuboid] = set()
+    init_region_only = init_region.stop < 10000
     for step in steps:
-        for x in step.x:
-            for y in step.y:
-                for z in step.z:
-                    vector = Vector3(x, y, z)
-                    if step.state:
-                        reactor.add(vector)
-                    elif vector in reactor:
-                        reactor.remove(vector)
-    
+        if init_region_only:
+            if step.x.start not in init_region or step.x.stop - 1 not in init_region:
+                continue
+            if step.y.start not in init_region or step.y.stop - 1 not in init_region:
+                continue
+            if step.z.start not in init_region or step.z.stop - 1 not in init_region:
+                continue
+
+        cuboid = Cuboid(step.x, step.y, step.z)
+        for c in reactor:
+            # TODO: determine if cuboid intersects c. if it does, dissect c by cuboid, insert the resulting cuboids into the set. if step.state == True, insert cuboid into the set too.
+            pass
+
     return len(reactor)
 
 def process_input(input: list[str]) -> list[Step]:
@@ -62,6 +71,6 @@ def process_input(input: list[str]) -> list[Step]:
     
     return steps
 
-solution = Solution(22, part1, part2, (590784, 0))
+solution = Solution(22, lambda input: solve(input, range(-50, 51)), solve, (590784, 0))
 solution.process_input(process_input)
 solution.run()
